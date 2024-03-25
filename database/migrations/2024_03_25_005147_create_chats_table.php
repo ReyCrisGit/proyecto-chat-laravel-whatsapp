1fac7c3adbdde9dca329dcd4f91f25a06898b4c7
Rey <reycrisbf@gmail.com>
2024-03-24 21:22:27 -0400

    Se realizaron las migraciones de accounts, contacts y chats

---
+++ database/migrations/2024_03_25_005147_create_chats_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('account_id');
+            $table->unsignedBigInteger('contact_id');
+            $table->string('message', 500);
+            $table->timestamp('date');
+            $table->boolean('seen')->default(false);
+            $table->timestamps();
+
+            $table->foreign('account_id')->references('id')->on('accounts')->onDelete('cascade');
+            $table->foreign('contact_id')->references('id')->on('contacts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chats');
+    }
+};
